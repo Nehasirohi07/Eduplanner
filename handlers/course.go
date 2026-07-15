ce692bbf35bbae8097817e150622e18abf7412e2
nehasirohi07 <nehasirohi665@gmail.com>
2026-07-15 16:42:05 +0530

    Implement course creation endpoint

--- handlers/course.go
+++ handlers/course.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"eduplanner/database"
 	"eduplanner/models"
 	"eduplanner/utils"
 )
@@ -27,5 +28,35 @@ func CreateCourse(w http.ResponseWriter, r *http.Request) {
 		utils.SendError(w, http.StatusBadRequest, err.Error())
 		return
 	}
+
+	userID, ok := r.Context().Value("userID").(int)
+
+	if !ok {
+		utils.SendError(w, http.StatusUnauthorized, "Invalid user")
+		return
+	}
+
+	course.UserID = userID
+
+	_, err = database.DB.Exec(
+		"INSERT INTO course(user_id, course_name)VALUES(? , ?)",
+		course.UserID,
+		course.CourseName,
+	)
+
+	if err != nil {
+		utils.SendError(
+			w,
+			http.StatusInternalServerError,
+			"Failed to create course",
+		)
+		return
+	}
+	utils.SendSuccess(
+		w,
+		http.StatusCreated,
+		"Course created successfully",
+		nil,
+	)
 
 }
